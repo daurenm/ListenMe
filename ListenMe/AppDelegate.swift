bab7474fb1c33bdfda75a004c4fc48a526703c30
Dauren <dauren.ktl@gmail.com>
2018-05-12 22:41:38 +0600

    Initial Commit

---
+++ ListenMe/AppDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  AppDelegate.swift
+//  ListenMe
+//
+//  Created by Dauren Muratov on 5/12/18.
+//  Copyright © 2018 paradox. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .white
+        let rootViewController = RootViewController()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+
+
+}
+
---
+++ ListenMe/RootViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RootViewController.swift
+//  ListenMe
+//
+//  Created by Dauren Muratov on 5/12/18.
+//  Copyright © 2018 paradox. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+    }
+
+
+}
+
