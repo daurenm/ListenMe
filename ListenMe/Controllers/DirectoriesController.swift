c604cfc251989ce59f787d963f22637d707a133d
Dauren <dauren.ktl@gmail.com>
2018-05-18 21:28:54 +0600

    add filemanager usage examples

--- ListenMe/Controllers/DirectoriesController.swift
+++ ListenMe/Controllers/DirectoriesController.swift
@@ -9,19 +9,40 @@
 import UIKit
 import EasyPeasy
 
+extension FileManager {
+    
+    static let documentsDirectory = `default`.urls(for: .documentDirectory, in: .userDomainMask)[0].path
+
+}
+
 class DirectoriesController: UIViewController {
     
+    // MARK: - Properties
+    var fileManager: FileManager { return FileManager.default }
+    
+    // MARK: - Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let fileManager = FileManager.default
-        print("current directory =", fileManager.currentDirectoryPath)
-        let dirPaths = fileManager.urls(for: .applicationDirectory, in: .userDomainMask)
-        let documentsDirectory = dirPaths[0].path
-        print(documentsDirectory)
-//        guard fileManager.changeCurrentDirectoryPath(documentsDirectory) else {
-//            assert(false, "Couldn't cd to 'Documents' directory of the app")
-//        }
-//        print(fileManager.currentDirectoryPath)
+        // changing current directory to documents directory
+        guard fileManager.changeCurrentDirectoryPath(FileManager.documentsDirectory) else {
+            assert(false, "Couldn't cd to 'Documents' directory of the app")
+        }
+        
+        // printing current directory files
+        let items = try? fileManager.contentsOfDirectory(atPath: fileManager.currentDirectoryPath)
+        print(items!)
+
+        // creating a file
+        let data = Data(capacity: 1024)
+        let success = fileManager.createFile(atPath: "dummyFile", contents: data, attributes: nil)
+        if success {
+            print("Successfully created a file")
+        } else {
+            print("Failed to create")
+        }
+        
+        let newItems = try? fileManager.contentsOfDirectory(atPath: fileManager.currentDirectoryPath)
+        print(newItems!)
     }
 }
